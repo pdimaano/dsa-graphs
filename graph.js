1189b6949d11d99bfc228b3e13e6a51cd5b1913b
Philip Dimaano <pdimaano7@gmail.com>
2022-12-15 12:14:03 -0800

    Started writing pseudocode for DFS method

--- graph.js
+++ graph.js
@@ -7,7 +7,6 @@ class Node {
   }
 }
 
-
 /** Graph class. */
 
 class Graph {
@@ -16,32 +15,56 @@ class Graph {
   }
 
   /** add Node instance and add it to nodes property on graph. */
-  addVertex(vertex) { }
+  addVertex(vertex) {
+    return this.nodes.add(vertex);
+  }
 
   /** add array of new Node instances and adds to them to nodes property. */
-  addVertices(vertexArray) { }
+  addVertices(vertexArray) {
+    for (let node of vertexArray) {
+      this.nodes.add(node);
+    }
+  }
 
   /** add edge between vertices v1,v2 */
-  addEdge(v1, v2) { }
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   /** remove edge between vertices v1,v2 */
-  removeEdge(v1, v2) { }
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   /** remove vertex from graph:
    *
    * - remove it from nodes property of graph
    * - update any adjacency lists using that vertex
    */
-  removeVertex(vertex) { }
+  removeVertex(vertex) {
+    for (let node of this.nodes) {
+      if (node === vertex) {
+        this.nodes.delete(node);
+        node.adjacent.delete(vertex);
+      }
+    }
+  }
 
   /** traverse graph with DFS and returns array of Node values */
-  depthFirstSearch(start) { }
+  depthFirstSearch(start) {
+    // Possibly use recursive helper function
+    // Keep array of everything we visited, outside of helper function
+    // Within function, can inspect one child node that isn't in visited array
+    // If in visited array
+  }
 
   /** traverse graph with BDS and returns array of Node values */
-  breadthFirstSearch(start) { }
+  breadthFirstSearch(start) {}
 
   /** find the distance of the shortest path from the start vertex to the end vertex */
-  distanceOfShortestPath(start, end) { }
+  distanceOfShortestPath(start, end) {}
 }
 
-module.exports = { Graph, Node }
+module.exports = { Graph, Node };
